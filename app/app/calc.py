bfbd035178ea5c5fd0e7c4c575fe51e540d80aef
philmaubois <philmaubois@gmail.com>
2023-01-15 16:11:47 +0100

    Change db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,12 @@
 Calculator function
 """
 
-def add(x,y):
+
+def add(x, y):
     """ Add x and y and return result """
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """ Subtract x and y and return result"""
     return y - x
-
